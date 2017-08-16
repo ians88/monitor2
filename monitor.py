a2afec2914dc1d63e13087e9dcc67a0ba90061f5
Ian scheele <ian@salescare.nl>
2017-08-16 14:23:12 +0200

    mysql

--- monitor.py
+++ monitor.py
@@ -4,6 +4,19 @@ import Adafruit_DHT
 
 import MySQLdb
 
+def truncateTable (tableName):
+    db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
+    cursor = db.cursor()
+
+    try:
+       cursor.execute("""TRUNCATE %s""",(tableName))
+       db.commit()
+    except:
+       db.rollback()
+
+    db.close()
+
+
 truncateTable("TempHumid")
 
 while True:
@@ -15,18 +28,6 @@ while True:
 
     try:
        cursor.execute("""INSERT INTO TempHumid VALUES (unix_timestamp(now()),%s,%s)""",(temperature,humidity))
-       db.commit()
-    except:
-       db.rollback()
-
-    db.close()
-
-def truncateTable (tableName):
-    db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
-    cursor = db.cursor()
-
-    try:
-       cursor.execute("""TRUNCATE %s""",(tableName))
        db.commit()
     except:
        db.rollback()
