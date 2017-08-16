45a10fb15472b6ad08839fcd158ea875cdb782aa
Ian scheele <ian@salescare.nl>
2017-08-16 14:17:41 +0200

    mysql

--- monitor.py
+++ monitor.py
@@ -2,7 +2,37 @@
 import sys
 import Adafruit_DHT
 
+import MySQLdb
+
+db = MySQLdb.connect(host= "localhost",
+                  user="root",
+                  passwd="password",
+                  db="monitoring")
+cursor = db.cursor()
+
+try:
+   cursor.execute("""TRUNCATE TempHumid""")
+   db.commit()
+except:
+   db.rollback()
+
+db.close()
+
 while True:
 	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
 	print 'Temp: {0:0.1f} C Humidity: {1:0.1f} %' .format(temperature, humidity)
+
+    db = MySQLdb.connect(host= "localhost",
+                      user="root",
+                      passwd="password",
+                      db="monitoring")
+    cursor = db.cursor()
+
+    try:
+       cursor.execute("""INSERT INTO TempHumid VALUES (unix_timestamp(now()),%s,%s)""",(temperature,humidity))
+       db.commit()
+    except:
+       db.rollback()
+
+    db.close()
 
