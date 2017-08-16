623399eb3778caf4836f15490b60c0c836ce2d5a
Ian scheele <ian@salescare.nl>
2017-08-16 14:41:13 +0200

    edit

--- monitor.py
+++ monitor.py
@@ -4,30 +4,43 @@ import Adafruit_DHT
 
 import MySQLdb
 
-print 'Truncating: TempHumid'
+import time
 
-db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
-cursor = db.cursor()
+counter = itertools.count().next()
 
-try:
-   cursor.execute("""TRUNCATE TempHumid""")
-   db.commit()
-except:
-   db.rollback()
+# print 'Truncating: TempHumid'
 
-db.close()
+# db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
+# cursor = db.cursor()
+
+# try:
+#    cursor.execute("""TRUNCATE TempHumid""")
+#    db.commit()
+# except:
+#    db.rollback()
+
+# db.close()
+
+updateTime = time.time()
+
+def minute_passed(oldepoch):
+    return time.time() - oldepoch >= 30
 
 while True:
     humidity, temperature = Adafruit_DHT.read_retry(11, 4)
     print 'Temp: {0:0.1f} C Humidity: {1:0.1f} %' .format(temperature, humidity)
 
-    db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
-    cursor = db.cursor()
+    if (minute_passed(time)):
+        time = time.time()
+        print 'Saving to DB'
 
-    try:
-       cursor.execute("""INSERT INTO TempHumid VALUES (unix_timestamp(now()),%s,%s)""",(temperature,humidity))
-       db.commit()
-    except:
-       db.rollback()
+        db = MySQLdb.connect(host= "localhost", user="root", passwd="password", db="monitoring")
+        cursor = db.cursor()
 
-    db.close()
+        try:
+           cursor.execute("""INSERT INTO TempHumid VALUES (unix_timestamp(now()),%s,%s)""",(temperature,humidity))
+           db.commit()
+        except:
+           db.rollback()
+
+        db.close()
